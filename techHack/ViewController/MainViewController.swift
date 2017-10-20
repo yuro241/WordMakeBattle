9c1cedac888c142d4639f130abea608c03bdab4d
Yuichiro Tsuji <Yuichiro_Tsuji@Yuichiros-Laptop.local>
2017-10-20 23:36:29 +0900

    レイアウト調整(仮)

--- techHack/ViewController/MainViewController.swift
+++ techHack/ViewController/MainViewController.swift
@@ -17,9 +17,10 @@ class MainViewController: APIRequest {
     @IBOutlet weak var deleteAllButton: UIButton!
     
     //この辺はbaseでまとめたい
-    let lettersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    let length: Int = 26
-    let buttonNum: Int = 20
+    let lettersArray = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Y","Z"]
+    let vowelsArray = ["A","E","I","O","U"]
+    let consonantButtonNum: Int = 10
+    let vowelButtonNum: Int = 5
     
     var answer: String = ""
     var timeCount: Float = 20.0
@@ -36,7 +37,8 @@ class MainViewController: APIRequest {
         deleteButton.addTarget(self, action: #selector(self.deleteAnswer), for: .touchUpInside)
         deleteAllButton.addTarget(self, action: #selector(self.deleteAllAnswer), for: .touchUpInside)
         
-        buttonSet(length:length, letters:lettersArray, count: buttonNum)
+        consonantButtonSet(length:lettersArray.count, letters:lettersArray, count: consonantButtonNum)
+        vowelButtonSet(length: vowelsArray.count, letters: vowelsArray, count: vowelButtonNum)
         setButtonDisable()
     }
     
@@ -46,26 +48,45 @@ class MainViewController: APIRequest {
         startTimer()
     }
     
-    func buttonSet(length: Int, letters: Array<String>, count: Int) {
-        for line in 1 ... count/10 {
-            for num in 0 ..< 10 {
-                let button = UIButton()
-                let rand = Int(arc4random()) % length
-                let randomAlphabet:String = letters[rand]
-                
-                button.setTitle(randomAlphabet, for: .normal)
-                button.setTitleColor(UIColor.black, for: .normal)
-                button.layer.borderWidth = 1.0
-                button.layer.borderColor = UIColor.black.cgColor
-                button.layer.cornerRadius = 8.0
-                button.addTarget(self, action: #selector(self.buttonEvent(sender:)), for: .touchUpInside)
-                button.sizeToFit()
-                self.view.addSubview(button)
-                
-                button.snp.makeConstraints{(make) in
-                    make.top.equalTo(self.view.snp.top).offset(100 + line * 75)
-                    make.left.equalTo(self.view.snp.left).offset(num * 30 + 30)
-                }
+    func consonantButtonSet(length: Int, letters: Array<String>, count: Int) {
+        for num in 0 ..< count {
+            let button = UIButton()
+            let rand = Int(arc4random()) % length
+            let randomAlphabet:String = letters[rand]
+            
+            button.setTitle(randomAlphabet, for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.layer.borderWidth = 1.0
+            button.layer.borderColor = UIColor.black.cgColor
+            button.layer.cornerRadius = 8.0
+            button.addTarget(self, action: #selector(self.buttonEvent(sender:)), for: .touchUpInside)
+            button.sizeToFit()
+            self.view.addSubview(button)
+            
+            button.snp.makeConstraints{(make) in
+                make.top.equalTo(self.view.snp.top).offset(175)
+                make.left.equalTo(self.view.snp.left).offset(num * 30 + 35)
+            }
+        }
+    }
+    
+    func vowelButtonSet(length: Int, letters: Array<String>, count: Int) {
+        for num in 0 ..< count {
+            let button = UIButton()
+            let Alphabet:String = letters[num]
+            
+            button.setTitle(Alphabet, for: .normal)
+            button.setTitleColor(UIColor.black, for: .normal)
+            button.layer.borderWidth = 1.0
+            button.layer.borderColor = UIColor.black.cgColor
+            button.layer.cornerRadius = 8.0
+            button.addTarget(self, action: #selector(self.vowelButtonEvent(sender:)), for: .touchUpInside)
+            button.sizeToFit()
+            self.view.addSubview(button)
+            
+            button.snp.makeConstraints{(make) in
+                make.top.equalTo(self.view.snp.top).offset(275)
+                make.left.equalTo(self.view.snp.left).offset(num * 60 + 55)
             }
         }
     }
@@ -77,6 +98,13 @@ class MainViewController: APIRequest {
         sender.isEnabled = false
         sender.layer.borderColor = UIColor.red.cgColor
         sender.setTitleColor(UIColor.red, for: .normal)
+        setButtonEnable()
+    }
+    
+    func vowelButtonEvent(sender: UIButton) {
+        print(sender.titleLabel!.text!)
+        answer = answer + sender.titleLabel!.text!
+        answerView.text = answer
         setButtonEnable()
     }
 
